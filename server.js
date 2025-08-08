f5729c2b70316d4be5c65f7f30735d01bcc36ac4
rarma4 <rarma4@gmail.com>
2025-08-07 22:57:02 -0300

    feat (cors) add lib cors

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from 'express'
+import cors from 'cors'
 import { PrismaClient } from './generated/prisma/index.js'
+
+
 
 const prisma = new PrismaClient()
 
 const app = express()
 app.use (express.json())
+app.use(cors())
 
 app.post('/usuarios', async (req, res) => {
     await prisma.user.create({

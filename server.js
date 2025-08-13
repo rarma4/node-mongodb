0aeaa93597c1b488f7def4351811c77309c3560b
rarma4 <rarma4@gmail.com>
2025-08-13 17:49:35 -0300

    feat (server.js) ajuste prismaclient

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import { PrismaClient } from './generated/prisma/index.js'
-
-
+// import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
